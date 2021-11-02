7eb51ef8ad8657699cc8e5cb6fb601a1188e5a90
eastriverlee <dan0720@naver.com>
2021-11-02 19:41:03 +0900

    Initial Commit

---
+++ ebagilbo/ebagilboApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebagilboApp.swift
+//  ebagilbo
+//
+//  Created by dan lee on 2021/11/02.
+//
+
+import SwiftUI
+
+@main
+struct ebagilboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
